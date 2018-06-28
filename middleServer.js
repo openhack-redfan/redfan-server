9fa7806869258c397700d1e509185c91ceb4588c
makehoney <makehoney3@gmail.com>
2018-06-29 01:46:40 +0900

    add express.js module

--- middleServer.js
+++ middleServer.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var app = express();
+const PORT = 2468;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -7,3 +10,8 @@ app.post('/sign_up', function(req, res) {
   // res.send
   // DB insert하기
 })
+
+
+app.listen(PORT, function() {
+  console.log("Middle Server is Running...")
+});
