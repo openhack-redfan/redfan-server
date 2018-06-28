8b7d802c85cdc91b464b756b77e6b14321359066
makehoney <makehoney3@gmail.com>
2018-06-29 03:15:04 +0900

    android 단에서 들어오는 json 형식 회원가입 정보를 post 방식으로 받아서 파싱하여 DB로 저장

--- middleServer.js
+++ middleServer.js
@@ -2,15 +2,36 @@ const PORT = 24680;
 
 var express = require('express');
 var app = express();
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+var mysql = require('mysql');
+var dbConnection = mysql.createConnection({
+  host: "13.209.8.64",
+  user: "teamRedFan",
+  password: "1234",
+  database: "redFan"
+})
+
 app.post('/sign_up', function(req, res) {
   console.log("received json sign_in data from android application");
-  // res.send
-  // DB insert하기
-})
+
+  var sql = "INSERT INTO users VALUES('" +
+            req.body.userId + "','" +
+            req.body.userPw + "','" +
+            req.body.userName + "','" +
+            req.body.channelUrl +
+            "')";
+
+  dbConnection.query(sql, function(err, row) {
+    if(err) throw err;
+
+    /* duplicate 정보가 들어왔을 때 (회원 아이디 중복시 예외처리 필요) */
+    res.send("inserted user information into databases successfully\n");
+  });
+});
 
 
 app.listen(PORT, function() {
